3f6f642cf450db30f081ae3c4d86f0666648382e
SYNC <eduardofreitasfr@gmail.com>
2024-12-03 10:29:29 -0300

    feat: paginas de controle

--- src/main/java/org/springframework/odontologico/controller/HomeController.java
+++ src/main/java/org/springframework/odontologico/controller/HomeController.java
@@ -1,14 +1,34 @@
 package org.springframework.odontologico.controller;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.odontologico.service.ClientsService;
+import org.springframework.odontologico.service.DoctorService;
+import org.springframework.odontologico.service.ExamsService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+    @Autowired
+    ClientsService serviceClient;
+    @Autowired
+    DoctorService serviceDoctor;
+    @Autowired
+    ExamsService serviceExam;
     @GetMapping("/")
     public String home(Model model) {
-        model.addAttribute("message", "Hello World");
+        model.addAttribute("totalClientsCount", serviceClient.countClients().toString());
+        model.addAttribute("totalExamsCount", serviceExam.getExams().toString());
         return "index";
+    }
+    @GetMapping("/registrar")
+    public String register( Model model) {
+        return "registercliente";
+    }
+    @GetMapping("/exames")
+    public String registerexam(Model model) {
+        model.addAttribute("clients", serviceClient.getAllClients());
+        model.addAttribute("doctors", serviceDoctor.getAllDoctors());
+        return "registerexam";
     }
 }
