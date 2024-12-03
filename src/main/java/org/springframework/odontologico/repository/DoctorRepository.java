2d775d5c837030f6d63fef2584065968e5abd5a7
SYNC <eduardofreitasfr@gmail.com>
2024-12-03 10:35:22 -0300

    feat: Adicionado manipulação de sql

---
+++ src/main/java/org/springframework/odontologico/repository/DoctorRepository.java
@@ -0,0 +1,7 @@
+package org.springframework.odontologico.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.odontologico.model.DoctorModel;
+
+public interface DoctorRepository extends JpaRepository<DoctorModel, Long> {
+}
