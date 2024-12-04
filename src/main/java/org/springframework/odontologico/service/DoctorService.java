98ce8e4212b58418b4404f0376b6025c6960201f
SYNC <eduardofreitasfr@gmail.com>
2024-12-04 17:51:23 -0300

    feaet serviços para doctor

--- src/main/java/org/springframework/odontologico/service/DoctorService.java
+++ src/main/java/org/springframework/odontologico/service/DoctorService.java
@@ -12,7 +12,27 @@ public class DoctorService {
     @Autowired
     private DoctorRepository doctorRepository;
 
+    public Long getCountDoctores(){
+        return doctorRepository.count();
+    }
+
+    // Retornar true caso existar um usuario registrado
+    private boolean findCpf(String cpf) {
+        DoctorModel doctor = doctorRepository.findByCpf(cpf);
+        if(doctor != null) {
+            return true;
+        }
+        return false;
+    }
     public List<DoctorModel> getAllDoctors() {
         return doctorRepository.findAll();
+    }
+    // Retorna true se o usuário for criado.
+    public boolean CreateDoctor(DoctorModel doctor) {
+        if(!findCpf(doctor.getCpf())) {
+            doctorRepository.save(doctor);
+            return true;
+        }
+        return false;
     }
 }
