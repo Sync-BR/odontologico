20f872d9b78fef4e7f3a7c2fb4120bde8aeee9ed
SYNC <eduardofreitasfr@gmail.com>
2024-12-04 17:51:09 -0300

    feat: repository doctor

--- src/main/java/org/springframework/odontologico/repository/DoctorRepository.java
+++ src/main/java/org/springframework/odontologico/repository/DoctorRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.odontologico.model.DoctorModel;
 
 public interface DoctorRepository extends JpaRepository<DoctorModel, Long> {
+    DoctorModel findByCpf(String cpf);
 }
